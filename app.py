d4d2a4de0c88e5e40ce94c635ba9b75146586d22
amrit2356 <amritanshupandey.meritocrat@gmail.com>
2024-09-10 15:38:21 +0530

    increase server timeout to 300 secs

--- app.py
+++ app.py
@@ -142,6 +142,6 @@ class SimpleLitAPI(ls.LitAPI):
 # (STEP 2) - START THE SERVER
 if __name__ == "__main__":
     # scale with advanced features (batching, GPUs, etc...)
-    server = ls.LitServer(SimpleLitAPI(), accelerator="auto", workers_per_device=10, api_path="/cocreation/predict")
+    server = ls.LitServer(SimpleLitAPI(), accelerator="auto", workers_per_device=10, api_path="/cocreation/predict", timeout=300)
     server.run(port=8000)
 
