024275f46335df6a83a33246199017a92d425347
amrit2356 <amritanshupandey.meritocrat@gmail.com>
2024-10-03 13:24:03 +0530

    updating app for testing

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from typing import List, Dict
 
 from pydantic import BaseModel
 
+from src.utils.config_manager import ConfigManager
 from src.utils.reader import ImageReader, resize_image
 from src.composition.image_harmonization.network.pctnet.net import PCTNet
 from src.composition.image_harmonization.network.palette.net.network import Palette
 from src.composition.image_processing.smoothening import BorderSmoothing
+from src.composition.shadow_generation.shadow_generator import ShadowGenerator
 from src.composition.utils.model_downloader import ModelDownloader
 from base_utils import *
 
@@ -28,6 +30,7 @@ PALETTE_MODEL_CONFIG = {'init_type': 'kaiming', 'module_name': 'guided_diffusion
 # (STEP 1) - DEFINE THE API (compound AI system)
 class SimpleLitAPI(ls.LitAPI):
     MODEL_PATH = "./"    
+
     def setup(self, device):
         self.device = device
         torch.backends.cudnn.enabled = True if torch.backends.cudnn.is_available() else False
@@ -42,18 +45,19 @@ class SimpleLitAPI(ls.LitAPI):
         self.foreground_image = None
         self.background_image = None
         self.border_smoothing = BorderSmoothing()
+        self.shadow_generator = ShadowGenerator()
 
         self.model_downloader_pctnet = ModelDownloader('pctnet', './')
         self.model_downloader_pctnet.download_models()
-        self.model_downloader_palette = ModelDownloader('palette', './')
-        self.model_downloader_palette.download_models()
+        # self.model_downloader_palette = ModelDownloader('palette', './')
+        # self.model_downloader_palette.download_models()
 
-        palette_model_path = './palette.pth'
-        self.palette = self.image_harmonization_models['palette'](**PALETTE_MODEL_CONFIG)
-        self.palette.load_state_dict(self.load_model(palette_model_path), strict=False)
-        self.palette.set_new_noise_schedule(phase='test')
-        self.palette.to(self.device)
-        self.palette.eval()
+        # palette_model_path = './palette.pth'
+        # self.palette = self.image_harmonization_models['palette'](**PALETTE_MODEL_CONFIG)
+        # self.palette.load_state_dict(self.load_model(palette_model_path), strict=False)
+        # self.palette.set_new_noise_schedule(phase='test')
+        # self.palette.to(self.device)
+        # self.palette.eval()
 
         pctnet_model_path = "./pctnet.pth"
         self.pctnet = self.image_harmonization_models['pctnet']()
@@ -83,9 +87,9 @@ class SimpleLitAPI(ls.LitAPI):
         context["foreground_image_url"] = request.foreground_img_url
         context["output_signed_url"] = request.output_signed_url
 
-        image_reader =  ImageReader("")
-        background_image = resize_image(image_reader.get_image_from_url(context['background_image_url']))
-        foreground_image = resize_image(image_reader.get_image_from_url(context['foreground_image_url']))
+        image_reader =  ImageReader()
+        background_image = resize_image(image_reader.get_image(context['background_image_url'], stream_type="url"))
+        foreground_image = resize_image(image_reader.get_image(context['foreground_image_url'], stream_type="url"))
 
         # Ensure the images have the same dimensions
         if foreground_image.shape[:2] != background_image.shape[:2]:
@@ -112,22 +116,25 @@ class SimpleLitAPI(ls.LitAPI):
 
     def predict(self, preprocessed_data: Tuple, context):
     # Easily build compound systems. Run inference and return the output.
+        harmonized_image = None
 
         if context['params']['model_type'] == 'pctnet':
             with torch.no_grad():
                 composite_frame, composite_mask, img_lr, img, mask_lr, mask = preprocessed_data
                 outputs = self.pctnet(img_lr, img, mask_lr, mask)
-                final_image = postprocess_pctnet(outputs)
+                harmonized_image = postprocess_pctnet(outputs)
         
         elif context['params']['model_type'] == 'palette':
             with torch.no_grad():
                 composite_frame, composite_mask, transformed_frame, transformed_mask = preprocessed_data
                 outputs, _ = self.palette.restoration(transformed_frame, transformed_frame, y_0=transformed_frame, mask=transformed_mask, sample_num=1)
-                final_image = postprocess_palette(composite_frame, transformed_frame, outputs)
+                harmonized_image = postprocess_palette(composite_frame, transformed_frame, outputs)
         else:
             raise ValueError("Model type not supported....")
         
-        final_image = self.border_smoothing.infer(final_image, composite_mask, self.background_image)
+        shadow_image = self.shadow_generator.infer(harmonized_image)
+        final_image = self.border_smoothing.infer(shadow_image, composite_mask, self.background_image)
+        
         return final_image
 
     def encode_response(self, final_image, context):
@@ -140,10 +147,10 @@ class SimpleLitAPI(ls.LitAPI):
             response = gcp_sent_status_code
             raise RuntimeError(message)
 
-        processed_message_status_code, message = send_process_confirmation(process_id)
-        if str(processed_message_status_code) != '200':
-            response = processed_message_status_code
-            raise RuntimeError(message)
+        # processed_message_status_code, message = send_process_confirmation(process_id)
+        # if str(processed_message_status_code) != '200':
+        #     response = processed_message_status_code
+        #     raise RuntimeError(message)
 
         return response
 
@@ -151,5 +158,5 @@ class SimpleLitAPI(ls.LitAPI):
 # (STEP 2) - START THE SERVER
 if __name__ == "__main__":
     # scale with advanced features (batching, GPUs, etc...)
-    server = ls.LitServer(SimpleLitAPI(), accelerator="auto", workers_per_device=10, api_path="/cocreation/predict", timeout=300)
-    server.run(port=8000)
+    server = ls.LitServer(SimpleLitAPI(), accelerator="cpu", workers_per_device=1, api_path="/cocreation/predict", timeout=300)
+    server.run(port=8000, generate_client_file=False)
